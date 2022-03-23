cdf07e3f66502be43c33f7db512c54bc378a1aaa
Rafael <rafaelmilanes91@gmail.com>
2022-03-23 18:49:02 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+//SELECTORS
+window.addEventListener("load", () =>{
+    console.log("use md5 module\nDownload it from here\nhttps://github.com/blueimp/JavaScript-MD5");
+
+    console.log("Copy all the hashes from the SQL exploit and crack them using\nhttps://hashes.com/en/tools/hash_identifier\n0")
+    console.log("Once cracked use md5 to verify that the passwords are correct by hashing them and checking if the current hashes are equivalent.")
+
+
+
+});
+
+
+
+
